f0c512c1186b83083111a74addd84bb06c907e68
adityarags <adityarags@gmail.com>
2023-08-12 10:39:28 +0530

    Initial App

---
+++ Frontend/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = None
+def create_app():
+    app = Flask(__name__, template_folder = 'templates')
+    app.app_context().push()
+
+    return app
+
+app = create_app()
+if __name__ == '__main__':
+    from app.controllers import *
+    app.run(debug = True)
